bf1c29c52b070e613ae44682b6bf9e33585ed8f1
Hayk <hayk.hayr3@gmail.com>
2020-12-04 01:11:59 +0400

    Add project files.

---
+++ OOP1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			MyClass instance = new MyClass();
+            Console.Write("Enter instance name: ");
+			instance.Name = Console.ReadLine();
+			Console.WriteLine(instance.Name);
+		}
+	}
+
+	class MyClass
+	{
+		private string name;
+		public string Name
+		{
+            get
+            {
+				if(name == "" || name == null)
+                {
+					return "Name is not defined";
+
+				}
+                else
+                {
+					return name;
+                }
+            }
+            set
+            {
+				name = value;
+            }
+		}
+
+	}
+}
